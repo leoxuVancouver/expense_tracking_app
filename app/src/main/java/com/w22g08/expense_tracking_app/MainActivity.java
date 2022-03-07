2de43226c20a90e1d307a1777053167fe7b9a18d
leoxuvancourver <leoguotengxu@gmail.com>
2022-03-06 18:13:14 -0800

    Initial commit

---
+++ app/src/main/java/com/w22g08/expense_tracking_app/MainActivity.java
@@ -0,0 +1,14 @@
+package com.w22g08.expense_tracking_app;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
